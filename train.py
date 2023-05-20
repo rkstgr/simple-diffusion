9e2912f11999fe687a12c5fcd5bf6f91db3946ff
Erik Steiger <rkstgr@gmail.com>
2023-05-20 09:47:38 +0200

    Add model summary and load from milestone

--- train.py
+++ train.py
@@ -1,15 +1,19 @@
 from simple_diffusion import UViT, GaussianDiffusion
 from trainer import Trainer
-
+from torchinfo import summary
 import argparse
 
 parser = argparse.ArgumentParser(description='Train diffusion model.')
 parser.add_argument('--image-path', type=str, required=True, help='Path to the directory containing training images.')
+parser.add_argument('--milestone', type=int, default=None, help='Load checkpoint at this milestone.')
 args = parser.parse_args()
 
 model = UViT(
-    dim=64
+    dim=16,
+    dim_mults=(1, 2, 4, 8),
 )
+
+summary(model, input_size=[(1, 3, 64, 64), (1,)], verbose=1)
 
 diffusion = GaussianDiffusion(
     model,
@@ -19,7 +23,7 @@ diffusion = GaussianDiffusion(
 trainer = Trainer(
     diffusion,
     args.image_path,
-    train_batch_size = 64,
+    train_batch_size = 128,
     train_lr = 1e-3,
     train_num_steps = 100_000,         # total training steps
     gradient_accumulate_every = 1,    # gradient accumulation steps
@@ -29,5 +33,7 @@ trainer = Trainer(
     calculate_fid = True              # whether to calculate fid during training
 )
 
+if args.milestone is not None:
+    trainer.load(args.milestone)
 
 trainer.train()
